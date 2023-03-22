058f41f155c520c55dc46deb8e1b4aad3cf04eb3
elbahaddi <elbachirhaddi@gmail.com>
2023-03-21 18:48:30 -0700

    khtkom

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: computes and prints even  number < 4,000,000
+ * Description: computes and prints even  number < 4000000
  * 5 below 1024 (excluded), followed by a new line
  * Return: 0
  */
